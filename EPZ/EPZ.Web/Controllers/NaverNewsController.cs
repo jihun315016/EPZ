379e57416f8c727d7472def55eb178229845c0cb
jihun <jihun315016@gmail.com>
2024-02-22 19:07:48 +0900

    네이버 뉴스 조회-샘플 코드 작성

--- EPZ/EPZ.Web/Controllers/NaverNewsController.cs
+++ EPZ/EPZ.Web/Controllers/NaverNewsController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EPZ.Web.Models;
+using EPZ.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Json;
 
 namespace EPZ.Web.Controllers
 {
+    [Route("[controller]")]
     public class NaverNewsController : Controller
     {
         public IActionResult Index()
@@ -9,8 +13,47 @@ namespace EPZ.Web.Controllers
             return View();
         }
 
-        //[HttpPost]
-        public IActionResult Get()
+        // https://localhost:7259/NaverNews/Get
+        [HttpPost]
+        [Route("Get")]
+        public async Task<IActionResult> GetAsync(NewsListVM<NaverNews> newsListVM)
+        {
+            string jsonString;
+            NewsListVM<NaverNews> list = new NewsListVM<NaverNews>();
+
+            if (newsListVM?.NewsQueryInfo == null) 
+            {
+                newsListVM = new NewsListVM<NaverNews>()
+                {
+                    NewsList = new List<NaverNews>(),
+                    NewsQueryInfo = new NewsQuery()
+                    {
+                        NewsCategory = NaverNewsCategory.언론사별.ToString(),
+                        PageSize = 10,
+                        Start = 1
+                    }
+                };
+            }
+
+            string url = "https://localhost:7259/api/NaverNews";
+            HttpClient client = new HttpClient();
+            var response = await client.PostAsJsonAsync<NewsListVM<NaverNews>>(url, newsListVM);
+
+            if (response.IsSuccessStatusCode) 
+            {
+                jsonString = await response.Content.ReadAsStringAsync();
+                list = Newtonsoft.Json.JsonConvert.DeserializeObject<NewsListVM<NaverNews>>(jsonString);
+                return View("Index", list);
+            }
+            else
+            {
+                return View("Index", null);
+            }
+        }
+
+        [HttpPost]
+        [Route("Hello")]
+        public IActionResult Hello(NewsListVM<NaverNews> newsListVM)
         {
             return View();
         }
--- EPZ/EPZ.Web/Interfaces/Services/INewsService.cs
+++ EPZ/EPZ.Web/Interfaces/Services/INewsService.cs
@@ -1,10 +1,11 @@
 ﻿using EPZ.Web.Interfaces.Models;
 using EPZ.Web.Models;
+using EPZ.Web.ViewModels;
 
 namespace EPZ.Web.Interfaces.Services
 {
     public interface INewsService
     {
-        public List<T> GetNewsList<T>(NewsQuery newsQuery) where T : INews;
+        public NewsListVM<T> GetNewsList<T>(NewsListVM<T> newsListVM) where T : INews;
     }
 }
